855d7a8f9222c0373b8a8461ed4e401f15a643cf
Sadhanakota <142041761+Sadhanakota@users.noreply.github.com>
2024-10-18 13:08:49 +0530

    Update App.java

--- src/main/java/Gmail/App.java
+++ src/main/java/Gmail/App.java
@@ -5,6 +5,6 @@ package Gmail;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello  how are you! ");
     }
 }
